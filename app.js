2c4f273fbd66e28eb6ad789bb83d65395a5d9311
ViviSotelo <vivi-nitro@hotmail.com>
2022-03-11 18:11:09 -0300

    trabajo terminado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views/index.html')));
 app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, 'views/babbage.html')));
-app.get('/barners-lee', (req,res) => res.sendFile(path.join(__dirname, 'views/barners-lee.html')));
+app.get('/berners-lee', (req,res) => res.sendFile(path.join(__dirname, 'views/berners-lee.html')));
 app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, 'views/clarke.html')));
 app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname, 'views/hamilton.html')));
 app.get('/hopper', (req,res) => res.sendFile(path.join(__dirname, 'views/hopper.html')));
